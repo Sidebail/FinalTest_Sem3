a27930d9ceda2f72ff672a54759fafedc71d487a
Sidebail <tfg.devteam@gmail.com>
2018-12-14 08:40:11 -0500

    Images are set, MenuLoader is implemented, FoodItem is created and set

---
+++ src/FoodItem.java
@@ -0,0 +1,75 @@
+import javafx.scene.image.Image;
+
+public class FoodItem implements Comparable<FoodItem> {
+
+    private String name;
+    private int calories;
+    private Image image;
+    private double price;
+
+    public FoodItem(String name, int calories, Image image, double price){
+        setName(name);
+        setCalories(calories);
+        setImage(image);
+        setPrice(price);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(!name.equals("")&&name.length()<=30){
+            this.name = name;
+        }else{
+            throw new IllegalArgumentException("Name has to be less tha 30 chars and cant be blank");
+        }
+
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        if(calories >= 0 && calories <= 3000){
+            this.calories = calories;
+        }else {
+            throw new IllegalArgumentException("Calories number has to be in range 0-3000");
+        }
+
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if(price >= 0 && price <= 50){
+            this.price = price;
+        }else {
+            throw new IllegalArgumentException("Price has to be in range of 0-50");
+        }
+
+
+    }
+
+    @Override
+    public int compareTo(FoodItem o) {
+        return name.compareTo(o.getName());
+    }
+
+    @Override
+    public String toString() {
+        return name + "    $" + String.format("%1$,.2f",price);
+    }
+}
---
+++ src/MenuLoader.java
@@ -0,0 +1,42 @@
+import javafx.scene.image.Image;
+
+import java.io.FileNotFoundException;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class MenuLoader {
+    public static TreeMap<String, TreeSet<FoodItem>> loadFoodItems() throws FileNotFoundException {
+        TreeMap<String, TreeSet<FoodItem>> menu = new TreeMap<>();
+
+        //FoodItem constructor should accept arguments in the order
+        //food name, calories, Image object, price
+
+        menu.put("beverages", new TreeSet<FoodItem>());
+        menu.get("beverages").add(new FoodItem("Strawberry Shake",500,new Image("./images/Strawberry-McCafe-Shake-Medium.png"),3.45));
+        menu.get("beverages").add(new FoodItem("Chocolate Shake",530,new Image("./images/Chocolate-McCafe-Shake-Medium.png"),3.45));
+        menu.get("beverages").add(new FoodItem("Coca Cola",150,new Image("./images/t-mcdonalds-Coca-Cola-Classic-Small.png"),1.79));
+        menu.get("beverages").add(new FoodItem("Sweet Iced Tea",90,new Image("./images/t-mcdonalds-Iced-Tea-Large.png"),2.59));
+
+        menu.put("breakfast", new TreeSet<FoodItem>());
+        menu.get("breakfast").add(new FoodItem("Hash Browns",150,new Image("./images/HASH-BROWNS.png"),1.69));
+        menu.get("breakfast").add(new FoodItem("Mcmuffin Triplestack",780,new Image("./images/mcmuffin-triplestack.png"),3.89));
+        menu.get("breakfast").add(new FoodItem("Fruit & Maple Oatmeal",310,new Image("./images/Fruit-Maple-Oatmeal.png"),4.79));
+        menu.get("breakfast").add(new FoodItem("Egg McMuffin",300,new Image("./images/Egg-McMuffin.png"),3.99));
+
+        menu.put("beef", new TreeSet<FoodItem>());
+        menu.get("beef").add(new FoodItem("Big Mac",520,new Image("./images/Big-Mac.png"),3.99));
+        menu.get("beef").add(new FoodItem("1/4 Pounder with Cheese",530,new Image("./images/Quarter-Pounder-with-Cheese.png"),3.79));
+
+        menu.put("chicken", new TreeSet<FoodItem>());
+        menu.get("chicken").add(new FoodItem("Artisan Grilled Chicken",510,new Image("./images/Artisan-Grilled-Chicken-Sandwich.png"),4.39));
+        menu.get("chicken").add(new FoodItem("Buttermilk Crispy Chicken",520,new Image("./images/Buttermilk-Crispy-Chicken-Deluxe-Sandwich.png"),4.49));
+
+
+
+
+
+
+
+        return menu;
+    }
+}
